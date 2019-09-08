422ac815db888cad3d3a9db35d3c17d19f33b47f
Keisuke Tsuji <kei.iwmmd@gmail.com>
2019-09-09 08:21:22 +0900

    CRUD機能を実装まで

--- src/blogs/views.py
+++ src/blogs/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+# myblog/blogs/views.py
+from django.urls import reverse_lazy
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+class IndexView(generic.ListView):
+    model = Post
+    paginate_by = 5
+    ordering = ['-updated_at']
+    template_name = 'blogs/index.html'
+
+class DetailView(generic.DetailView):
+    model = Post
+    template_name = 'blogs/detail.html'
+
+class CreateView(generic.edit.CreateView):
+    model = Post
+    fields = '__all__'  # ['title', 'text', 'author']
+    success_url = reverse_lazy('blogs:index')
+    template_name = 'blogs/create.html'
+
+class UpdateView(generic.edit.UpdateView):
+    model = Post
+    fields = '__all__'  # ['title', 'text', 'author']
+    success_url = reverse_lazy('blogs:index')
+    template_name = 'blogs/update.html'
+    
+
+class DeleteView(generic.edit.DeleteView):
+    model = Post
+    success_url = reverse_lazy('blogs:index')
+    template_name = 'blogs/delete.html'
